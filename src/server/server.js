4ccce69cb45b12326151838e85990f5fc6793bf1
supss03 <myusufalhafiz@gmail.com>
2024-12-13 18:24:24 +0700

    update sc for deploy

--- src/server/server.js
+++ src/server/server.js
@@ -6,7 +6,7 @@ const InputError = require('../exceptions/InputError');
 
 (async () => {
     const server = Hapi.server({
-        port: 3000,
+        port: process.env.PORT || 8000,
         host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
         routes: {
             cors: {
